a97bca83e4c2e8625c72a726fac25eead66c1f6b
shashilsravan <shashilsravan.ss.ss@gmail.com>
2021-01-20 08:54:10 +0530

    favicon

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -123,7 +123,7 @@ export default function Header() {
                 </div>
             </div>
             <div className="header-mobile">
-                <div className="container-xl">
+                <div className="container-xl my-2">
                     <div className="header-mobile__toggle-sidebar">
                         <button><i className="fa fa-bars"></i></button>
                     </div>
