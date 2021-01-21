a512759e7462f0ef1b585d4c37fee142f76e131f
shashilsravan <shashilsravan.ss.ss@gmail.com>
2021-01-21 20:07:10 +0530

    photo size

--- frontend/src/minicomponents/NoItems.js
+++ frontend/src/minicomponents/NoItems.js
@@ -5,7 +5,9 @@ export default function NoItems({text}) {
     return (
         <div>
             <div className="noItemsImage d-flex flex-column align-items-center">
-                <img src="https://cdn2.iconfinder.com/data/icons/ecommerce-outline-6/32/Ecommerce-shopping-sale-transaction_21-512.png" alt=""/>
+                <img 
+                height="150" width="150"
+                src="https://cdn2.iconfinder.com/data/icons/ecommerce-outline-6/32/Ecommerce-shopping-sale-transaction_21-512.png" alt=""/>
                 <h3>{text}</h3>
                 <Link to='/' className="btn btn-block btn-light mt-2 mb-5">
                     Keep Shopping 
